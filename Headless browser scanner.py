ffe6656a8fa600a3fc60d51218c4319649992f4f
Elia (auf PC) <charefe@bzz.ch>
2026-07-06 23:01:14 +0200

    Added a new function to restrict link gathering to a depth of one. Note: Future task to add inline comments and further refine user experience.

--- Headless browser scanner.py
+++ Headless browser scanner.py
@@ -1,11 +1,22 @@
 from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+
+
+def get_links(html):
+    soup = BeautifulSoup(html, "html.parser")
+    links = []
+    for anchor in soup.find_all("a", href=True):
+        links.append(anchor['href'])
+    return links
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
 
-    page.goto("https://www.youtube.com/")
-    print("Page Title:", page.title())
+    page.goto("https://en.wikipedia.org/wiki/Main_Page")
+    html_content = page.content()
+    extracted_links = get_links(html_content)
+    print(f"Page Title: {page.title()}\nlinks: {extracted_links}")
 
     page.screenshot(path="example.png")
 
